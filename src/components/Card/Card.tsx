306480ea8efd0afc638c40192ba6f77159c45f43
anton fediienko <anton.fediienko@pm.bet>
2024-08-18 21:59:51 +0200

    handle loading and error

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,47 +1,63 @@
-import React, { FC } from 'react';
+import { ReactNode } from 'react';
+
+import { Loader } from '../Loader';
 
 import { Table } from './Table/Table';
+import { Column } from './Table/typings';
 import styles from './Card.module.css';
 
 type Props<T> = {
   title: string;
   items: T[];
-  isPermitted: boolean;
   isError: boolean;
   isLoading: boolean;
-  columns: any[];
   layout: 'list' | 'table';
-  link?: {
-    label: string;
-    url: string;
-  };
+  columns?: Column[];
+  footer?: ReactNode;
 };
 
-export const Card = <T,>({
+export const Card = <
+  T extends {
+    [ket: string]: any;
+  },
+>({
   title,
   items,
-  isPermitted,
   isLoading,
+  isError,
   layout,
-  link,
   columns,
+  footer,
 }: Props<T>) => {
-  console.log(items);
+  const renderBody = () => {
+    if (isLoading) {
+      return <Loader />;
+    }
+
+    if (isError) {
+      return (
+        <div className={styles.error}>
+          Unable to fetch data. Try again in a few moments.
+        </div>
+      );
+    }
+
+    return (
+      <>
+        {layout === 'table' && columns?.length && (
+          <Table<T> data={items} columns={columns} />
+        )}
+      </>
+    );
+  };
 
   return (
     <div className={styles.container}>
-      {isLoading ? (
-        <div>Loading...</div>
-      ) : (
-        <>
-          <h3 className={styles.title}>{title}</h3>
-          <div className={styles.body}>
-            {layout === 'table' && columns.length && (
-              <Table data={items} columns={columns} />
-            )}
-          </div>
-        </>
-      )}
+      <header>
+        <h3 className={styles.title}>{title}</h3>
+      </header>
+      <main className={styles.body}>{renderBody()}</main>
+      {footer && <footer>{footer}</footer>}
     </div>
   );
 };
